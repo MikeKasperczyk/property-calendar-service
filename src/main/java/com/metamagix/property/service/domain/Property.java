07d7331964b8beb951b1eb125aa06e494787c2ce
Mike Kasperczyk <mike.kasperczyk@gmail.com>
2017-03-06 23:29:06 +0100

    added new fields for property

--- src/main/java/com/metamagix/property/service/domain/Property.java
+++ src/main/java/com/metamagix/property/service/domain/Property.java
@@ -2,6 +2,9 @@ package com.metamagix.property.service.domain;
 
 public class Property extends AbstractEntity {
     String address;
+    String zipCode;
+    String city;
+    String country;
 
     public String getAddress() {
         return address;
@@ -9,5 +12,29 @@ public class Property extends AbstractEntity {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 }
